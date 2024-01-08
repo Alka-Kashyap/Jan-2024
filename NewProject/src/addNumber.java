26f510bffa46bbbbeead10ac7473c4dde379fa95
Ashish <Ashish@DESKTOP-3TB52GH>
2024-01-08 16:32:50 +0530

    final commit

---
+++ NewProject/src/addNumber.java
@@ -0,0 +1,16 @@
+
+public class addNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int num1 = 5;
+ int num2 = 5;
+ int num3 = 5;
+ int add;
+ add = num1+num2+num3;
+ System.out.println("Total sum is: " + add);
+ 
+		 
+	}
+
+}
