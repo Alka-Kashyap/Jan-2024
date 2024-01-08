df76f2c7cd2d750118d4683f8375a9a480bfaa56
Ashish <Ashish@DESKTOP-3TB52GH>
2024-01-08 16:38:18 +0530

    final commit

---
+++ NewProject/src/TestingRelational.java
@@ -0,0 +1,30 @@
+
+public class TestingRelational {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		//boolean c = true;
+		
+//if (a==b || a==c) 
+//{
+//	System.out.println("True");
+//}
+//else if (a==b || b==c) {
+//
+//	System.out.println("True");
+//}	
+//else 
+//{
+//
+//	System.out.println("False");
+//}
+//System.out.print("Logical operators");
+		System.out.println("for Logical NOT");
+		System.out.print("Not(A) is: ");
+		System.out.println(!a);
+		System.out.print("Not(B) is: ");
+		System.out.println(!b);
+	}
+
+}
---
+++ NewProject/src/Testing_2_elseif.java
@@ -0,0 +1,8 @@
+
+public class Testing_2_elseif 
+{
+	public static void main(String[] args) {
+		
+	}
+
+}
